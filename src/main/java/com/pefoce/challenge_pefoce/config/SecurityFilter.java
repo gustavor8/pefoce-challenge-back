7155c866ca95668610c546b9fed1b4f0cf3a3d37
Gustavo Rodrigues <121266795+gustavor8@users.noreply.github.com>
2025-10-05 21:32:56 -0300

    Bugfix/authentication (#13)
    * Feat: Added chain of custody for a trace transfer
    
    * Bugfix: fixed error in the login with invalid token

--- src/main/java/com/pefoce/challenge_pefoce/config/SecurityFilter.java
+++ src/main/java/com/pefoce/challenge_pefoce/config/SecurityFilter.java
@@ -1,53 +1,72 @@
 package com.pefoce.challenge_pefoce.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper; // Para converter o erro em JSON.
+import com.pefoce.challenge_pefoce.dto.shared.ErrorResponseDTO;
+import com.pefoce.challenge_pefoce.entity.Users;
 import com.pefoce.challenge_pefoce.repository.UserRepository;
 import com.pefoce.challenge_pefoce.service.util.TokenService;
-// Define a cadeia de filtros por onde uma requisição passa.
 import jakarta.servlet.FilterChain;
-// Exceção que pode ocorrer durante o processamento de um filtro.
 import jakarta.servlet.ServletException;
-// Representa uma requisição HTTP.
 import jakarta.servlet.http.HttpServletRequest;
-// Representa uma resposta HTTP.
 import jakarta.servlet.http.HttpServletResponse;
-
-import org.springframework.beans.factory.annotation.Autowired;
-// Objeto que representa o usuário autenticado, suas permissões e credenciais.
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;    // Para definir o Content-Type.
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-// Classe central que armazena os detalhes de segurança da requisição atual.
 import org.springframework.security.core.context.SecurityContextHolder;
-// Interface que define os dados de um usuário.
-import org.springframework.security.core.userdetails.UserDetails;
-// Marca a classe como um componente gerenciado pelo Spring.
 import org.springframework.stereotype.Component;
-// Classe base do Spring para garantir que um filtro seja executado apenas uma vez por requisição.
 import org.springframework.web.filter.OncePerRequestFilter;
 
-// Exceção relacionada a operações de entrada/saída.
 import java.io.IOException;
+import java.io.OutputStream;
+import java.time.Instant;
+import java.util.Optional;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
-  @Autowired
-  private TokenService tokenService;
-  @Autowired
-  private UserRepository userRepository;
+  private final TokenService tokenService;
+  private final UserRepository userRepository;
+
+  public SecurityFilter(TokenService tokenService, UserRepository userRepository) {
+    this.tokenService = tokenService;
+    this.userRepository = userRepository;
+  }
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
     throws ServletException, IOException {
-    String token = recoverToken(request);
 
-    if (token!=null) {
-      String username = tokenService.validateToken(token);
-      var userOptional = userRepository.findByUsername(username);
-      userRepository.findByUsername(username).ifPresent(user -> {
-        var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-      });
+    try {
+      var token = recoverToken(request);
+
+      if (token!=null) {
+        var username = tokenService.validateToken(token);
+        Optional<Users> userOptional = userRepository.findByUsername(username);
+        userOptional.ifPresent(user -> {
+          var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+          SecurityContextHolder.getContext().setAuthentication(authentication);
+        });
+      }
+
+      filterChain.doFilter(request, response);
+
+    } catch (RuntimeException e) {
+
+      ErrorResponseDTO errorResponse = new ErrorResponseDTO(
+        Instant.now(),
+        HttpStatus.FORBIDDEN.value(), // Status 403
+        "Token Inválido",
+        "O token de acesso é inválido ou expirado. Por favor, autentique-se novamente.",
+        request.getRequestURI()
+      );
+
+      response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+      response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+      OutputStream responseStream = response.getOutputStream();
+      ObjectMapper mapper = new ObjectMapper().findAndRegisterModules();
+      mapper.writeValue(responseStream, errorResponse);
+      responseStream.flush();
     }
-    filterChain.doFilter(request, response);
   }
 
   private String recoverToken(HttpServletRequest request) {
